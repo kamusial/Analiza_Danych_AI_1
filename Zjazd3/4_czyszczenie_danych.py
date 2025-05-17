12d1c6d53b597c8ed59b8fcc0275a1990cae6dfd
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-05-17 12:28:40 +0200

    '1'

--- Zjazd3/4_czyszczenie_danych.py
+++ Zjazd3/4_czyszczenie_danych.py
@@ -1,7 +1,18 @@
-import kagglehub
+import pandas as pd
 
-# Download latest version
-path = kagglehub.dataset_download("ahmedmohamed2003/retail-store-sales-dirty-for-data-cleaning")
+df = pd.read_csv('data\\retail_store_sales.csv')
 
-print(path)
-print(type(path))
+print(f'Kształt danych: {df.shape}')
+print(f'Nazwy kolumn: {df.columns}')
+print(f'Pięć pierwszych wierzy: {df.head().to_string()}')
+print(df.describe().T.to_string())
+
+duplikaty = df.duplicated().sum()
+print(f"Liczba duplikatów przed usunięciem: {duplikaty}")
+df = df.drop_duplicates()
+print(f"Liczba wierszy po usunięciu duplikatów: {df.shape[0]}")
+
+df['Category'] = df['Category'].str.strip()
+df['Category'] = df['Category'].str.capitalize()
+print(f'Pięć pierwszych wierzy po czyszczeniu: {df.head().to_string()}')
+
