0a6702bae4a380c943fce574fbad2ed3ed681ba2
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-05-17 10:46:34 +0200

    '1'

--- Zjazd3/1_csv_bez_bibliotek.py
+++ Zjazd3/1_csv_bez_bibliotek.py
@@ -10,7 +10,24 @@ print(content[7][5:9])
 
 for i in range(len(content)):
     content[i] = content[i].split(',')
-
 print(content)
+print(content[5][3])
 
+# 2. Obliczenie sredniej wyplaty
+total = 0
+for i in range(1, len(content)):   # omijamy 1wszą linię
+#    print(f'Wyplata: {content[i][1]}')
+    total += int(content[i][1])
+average_salary = total / (len(content)-1)
+print(f'średnia wypłata: {round(average_salary, 2)}')
 
+# 3. Obliczenie liczby kobiet na macierzynskim
+total = 0
+for i in range(1, len(content)):
+#     content[i][4] = content[i][4].replace('\n','')
+#    print(f'Macierzynski: {content[i][4]}')
+    if content[i][4][0].lower() == 't' and content[i][3] == 'k':
+        total += 1
+print(f'Liczba Pań na macierzyńskim: {total}')
+
+# string.replace('a','')
