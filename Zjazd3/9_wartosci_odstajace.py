3541b155162926491c405c81341d3d2a73c4e615
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-05-18 11:21:09 +0200

    '1'

--- Zjazd3/9_wartosci_odstajace.py
+++ Zjazd3/9_wartosci_odstajace.py
@@ -8,14 +8,14 @@ df = pd.read_csv('data\\otodom.csv')
 print(df.describe().T.round(2).to_string())
 
 
-sns.heatmap(df.iloc[  :  ,     2:   ].corr(), annot=True)   # pomiń 2 pierwsze kolumny
-plt.show()
-
-sns.histplot(df.cena)
-plt.show()
-
-plt.scatter(df.powierzchnia, df.cena)
-plt.show()
+# sns.heatmap(df.iloc[  :  ,     2:   ].corr(), annot=True)   # pomiń 2 pierwsze kolumny
+# plt.show()
+#
+# sns.histplot(df.cena)
+# plt.show()
+#
+# plt.scatter(df.powierzchnia, df.cena)
+# plt.show()
 
 q1 = df.describe().T.loc["cena", "25%"]
 q3 = df.describe().T.loc["cena", "75%"]
@@ -24,10 +24,19 @@ print(q1, q3)
 df1 = df[(df.cena >= q1) & (df.cena <= q3)  ]
 print(f'ksztalt {df1.shape}')
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
 X = df1.iloc[:, 2:]
 y = df1.cena
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+r2 = r2_score(y_test,  model.predict(X_test)  )
+print(f'r2: {r2}')
+
+print(pd.DataFrame(model.coef_ , X.columns))
+print(f'wyraz wolny: {model.intercept_}')
